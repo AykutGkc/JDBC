ad3502705d81177049effdbc0ff3409da9a46adb
Aykut Gökce <aykutgokce2016@gmail.com>
2023-03-14 11:33:59 +0100

    day04

---
+++ src/main/java/Query08.java
@@ -0,0 +1,17 @@
+import static utilities.DatabaseUtilty.*;
+
+public class Query08 {
+    public static void main(String[] args) {
+
+        createConnection();
+
+        String sql01="select * from ogrenciler";
+        System.out.println("Sütun isimleri "+getColumnNames(sql01));
+
+        System.out.println("Okul No: "+getColumnData(sql01,"okul_no"));
+        System.out.println("Ögrenci ismi: "+getColumnData(sql01,"ogrenci_ismi"));
+        System.out.println("Sinif: "+getColumnData(sql01,"sinif"));
+
+
+    }
+}
